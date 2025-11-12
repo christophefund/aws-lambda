494ff3da5ee1866fa56a00c0efa7b1ca9e7251c8
christophefund <boldnightowl@gmail.com>
2025-11-12 17:17:46 +0100

    Added EC2 related Function

---
+++ EC2/stop_instance.py
@@ -0,0 +1,13 @@
+
+# This function stops a given EC2 instance (Shouldn't be hardcoded though but received via a trigger)
+
+import boto3
+#This simple lambda function is available from AWS with instructions on starting and stopping an instance at regular intervals using Lambda and CloudWatch: https://aws.amazon.com/premiumsupport/knowledge-center/start-stop-lambda-cloudwatch/
+# Enter the region your instances are in. Include only the region without specifying Availability Zone; e.g., 'us-east-1'
+region = 'us-east-1'
+# Enter your instances here: ex. ['X-XXXXXXXX'] you can comma separate the instance IDs for more than one instance: i.e. ['X-XXXXXXXXX', 'X-XXXXXXXXX"]
+instances = ['i-0902effe70a087ae7']
+
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2', region_name=region)
+    ec2.stop_instances(InstanceIds=instances)
